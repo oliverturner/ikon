57aca64cd0a7b7d94210a2aa359a72fae5a3c847
Oliver Turner <oliver.turner@codedsignal.co.uk>
2020-10-10 11:22:59 +0100

    Parse entries as a structured object

---
+++ app/js/utils.js
@@ -0,0 +1,53 @@
+/**
+ * @param {FileSystemFileEntry} entry
+ * @returns {Promise<string | ArrayBuffer>}
+ */
+export function getText(entry) {
+  return new Promise(function (resolve, reject) {
+    entry.file(function (file) {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = () => reject(reader.error);
+      reader.readAsText(file);
+    }, reject);
+  });
+}
+
+/**
+ * @param {FileSystemFileEntry} entry
+ * @returns {Promise<string>}
+ */
+export function getType(entry) {
+  return new Promise(function (resolve, reject) {
+    entry.file(function (file) {
+      resolve(file.type);
+    }, reject);
+  });
+}
+
+/**
+ * @param {string} svgString
+ */
+export function createSVG(svgString) {
+  const fragment = document.createElement("div");
+  fragment.innerHTML = svgString;
+  return fragment.querySelector('svg');
+}
+
+// Type guards
+//--------------------------------------------------------------------------------------------------
+/**
+ * @param {FileSystemEntry} entry 
+ * @returns {entry is FileSystemDirectoryEntry}
+ */
+export function isDirectory(entry) {
+  return entry.isDirectory;
+}
+
+/**
+ * @param {FileSystemEntry} entry 
+ * @returns {entry is FileSystemFileEntry}
+ */
+export function isFile(entry) {
+  return entry.isFile;
+}
